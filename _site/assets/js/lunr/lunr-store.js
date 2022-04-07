a9f136c7ca3633f4891f52d8cf250a393665a131
Hyejic <hyeji0314@gmail.com>
2022-04-07 16:34:47 +0900

    04장 기록중

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": ["Markdown"],
         "url": "/md/markdown/",
         "teaser": null
+      },{
+        "title": "04장 변수",
+        "excerpt":"04장 변수 4.1 변수란 무엇인가? 왜 필요한가? 변수란? 변수는 하나의 값을 저장하기 위해 확보한 메모리 공간 자체 또는 그 멤모리 공간을 식별하기 위해 붙인 이름이다. var userId = 1; var userName = 'Lee'; // 객체나 배열 같은 자료구조를 사용하면 여러개의 값을 하나로 그룹화해서 하나의 값처럼 사용할 수 있다. var user...","categories": ["js"],
+        "tags": ["javascript"],
+        "url": "/js/modernJS04/",
+        "teaser": null
       }]
